14215ddb7abb575465de49d4fa2311faa014e006
nikitamulia <crovone96@gmail.com>
2022-11-09 11:51:47 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import { renderStartMarkup } from './js/renderStartMarkup';
+<<<<<<< Updated upstream
+=======
+import './js/modal';
+import'./js/header-mobile';
+>>>>>>> Stashed changes
 
 renderStartMarkup();
