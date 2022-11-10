8e18e16ef7b31ebd59e724c2092f1780550490b2
Andrii Zorba <37942049+Zorbik@users.noreply.github.com>
2022-11-10 16:44:28 +0200

    123

--- src/js/renderSearchPage.js
+++ src/js/renderSearchPage.js
@@ -1,19 +1,17 @@
-// import { createMarkupCocktail } from './createMarkupCocktail';
-// import { gallery } from './renderStartMarkup';
+import { createMarkupCocktail } from './createMarkupCocktail';
+import { gallery } from './refs';
 
-// export function pagGallery(array, cardsPerPage, page) {
-//   const newArray = array.splice(cardsPerPage * page - 1, page);
-//   gallery.innerHTML = createMarkupCocktail(newArray);
-// }
+export function pagGallery(array, cardsPerPage, page) {
+  const newArray = array.splice(cardsPerPage * page - 1, page);
+  gallery.innerHTML = createMarkupCocktail(newArray);
+}
 
-// export function numPagesGallery(number) {
-//   const markup = '<div class="numPage__container"><div class="decrPage"></div>';
-//   for (let i = 1; i <= number; i += 1) {
-//     markup += `<div class="numPage">${i}</div>`;
-//   }
-//   markup += '<div class="incrPage"></div></div>';
-// }
+export function numPagesGallery(number) {
+  const markup = '<div class="numPage__container"><div class="decrPage"></div>';
+  for (let i = 1; i <= number; i += 1) {
+    markup += `<div class="numPage">${i}</div>`;
+  }
+  markup += '<div class="incrPage"></div></div>';
+}
 
-//
-// ___________________________________________________
-//
+___________________________________________________;
