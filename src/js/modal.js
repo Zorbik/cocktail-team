dd7b51d81b377aee2a25eae89e023d69f090b55c
AndreyLytvynov <mrjastonehome@gmail.com>
2022-11-10 11:06:27 +0200

    Модалка, список ингридиентов

---
+++ src/js/modal.js
@@ -0,0 +1,53 @@
+import { getCocktailById } from './request-api';
+import { createModalMarkupCocktail } from './createModalCoctailMarkup';
+import { isFavorite } from './addFavoriteCocktail';
+
+const closeModalButton = document.querySelector('.js-modal-coctail-close');
+const modalCardEl = document.querySelector('#modal');
+const body = document.querySelector('body');
+const gallery = document.querySelector('.gallery');
+const overlay = document.getElementById('overlay');
+
+gallery.addEventListener('click', onGalleryClick);
+overlay.addEventListener('click', onOverlayClick);
+// closeModalButton.addEventListener('click', onCloseButtonClick);
+
+async function onGalleryClick(e) {
+  if (!e.target.dataset.modalTarget) {
+    return;
+  }
+  openModal(modalCardEl);
+  console.log(e.target.dataset.id);
+  const coctail = await getCocktailById(e.target.dataset.id);
+
+  const markup =  await createModalMarkupCocktail(coctail.drinks);
+  modalCardEl.innerHTML = markup;
+
+  modalCardEl.addEventListener('click', e => {
+    isFavorite(e);
+  });
+}
+
+// function onCloseButtonClick() {
+//   closeModal(modalCardEl);
+//   console.log('azaz');
+// }
+
+function onOverlayClick() {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach(modalCardEl => {
+    closeModal(modalCardEl);
+  });
+}
+
+function openModal(modalCardEl) {
+  modalCardEl.classList.add('active');
+  overlay.classList.add('active');
+  body.classList.toggle('modal-opened');
+}
+
+function closeModal(modalCardEl) {
+  modalCardEl.classList.remove('active');
+  overlay.classList.remove('active');
+  body.classList.toggle('modal-opened');
+}
