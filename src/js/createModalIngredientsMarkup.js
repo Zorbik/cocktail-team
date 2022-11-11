922cbf1681943a9f3abf20934707ab19da32eca5
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-11-11 18:17:12 +0200

    added all mobile styles to 2nd modal

--- src/js/createModalIngredientsMarkup.js
+++ src/js/createModalIngredientsMarkup.js
@@ -2,7 +2,7 @@ import sprite from '../images/symbol-defs.svg';
 import { checkIdIngredients } from './addFavoriteIngredient';
 
 export async function createModalMarkupCocktail(arrCocktails) {
-  const { idIngredient, strIngredient, strType, strAlocohol, strDescription } = arrCocktails;
+  const { idIngredient, strIngredient, strType, strAlcohol, strDescription } = arrCocktails;
   return `
         <button type="button" class="modal__cocktail-close-button">
     <svg class="close__icon" width="32" height="32">
@@ -12,7 +12,9 @@ export async function createModalMarkupCocktail(arrCocktails) {
   <h2 class="modal__ingredient-name">${strIngredient}</h2>
   <p class="modal__ingredient-type">${strType ? strType : ''}</p>
   <p class="modal__ingredient-description">${strDescription ? strDescription : ''}</p>
-  <ul class="modal__ingredient-info"></ul>
+  <ul class="modal__ingredient-info">
+    <li>✶ <span modal__ingredient-item>Alcohol: ${strAlcohol ? strAlcohol : ''}</span></li>
+  </ul>
   <button class="gallery__add-btn btn ${
     checkIdIngredients(idIngredient) >= 0 ? 'icon-ingredients-fill' : ''
   }
