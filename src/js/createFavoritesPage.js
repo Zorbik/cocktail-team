b0a18455de16a62fa03a0afcb2060c8f2f73793a
UsernameViktoriia <tori-boss@ukr.net>
2022-11-11 14:36:53 +0200

    створено файли для сторінки favoriteCocktails

--- src/js/createFavoritesPage.js
+++ src/js/createFavoritesPage.js
@@ -20,12 +20,11 @@ function generateArrPromiseForFavoriteCocktailsMarkup() {
   return arrPromise;
 }
 
-async function renderFavoriteCocktailsMarkup() {
+export async function renderFavoriteCocktailsMarkup() {
   const arrPromise = generateArrPromiseForFavoriteCocktailsMarkup();
   const arrCocktails = await Promise.all([...arrPromise]);
   console.log('arrCocktails', arrCocktails);
   const markup = await createMarkupCocktail(arrCocktails);
   document.querySelector('.gallery__list').innerHTML = markup;
   document.querySelector('.gallery__title').textContent = 'Favorite Cocktails';
-  document.querySelector('.hero').classList.add('js_hero_hidden');
 }
